4f4cb13a99148100b2921fa36096bf84542cc232
lucas-fochesatto <lucas.fochesatto@gmail.com>
2021-12-21 20:39:32 -0400

    Aula 00

---
+++ Aula 00/HelloWorld.java
@@ -0,0 +1,34 @@
+//************************************************************ 
+//Lucas Fochesatto e Victor Oliva - Curso Introdutório de Robótica para OBR
+//Créditos ao autor Wei Lu pela criação do código
+//Referência: Beginning Robotics Programming in Java with LEGO Mindstorms
+//Exemplo para escrever Hello World no display do EV3 brick
+//************************************************************
+
+package aplicacao;
+
+//importar pacotes de hardware EV3 para encontrar o brick,
+//acessar os botões e o display LCD
+import lejos.hardware.ev3.EV3;
+import lejos.hardware.BrickFinder; 
+import lejos.hardware.Keys;
+import lejos.hardware.lcd.TextLCD;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// encontrar o EV3 brick
+		EV3 ev3brick = (EV3) BrickFinder.getLocal();
+		
+		// instanciar a classe LCD para o display
+		// instanciar a classe Keys para pegar os botões do brick
+		Keys buttons = ev3brick.getKeys();
+		TextLCD display = ev3brick.getTextLCD();
+		
+		// desenhar texto no display baseado nas coordenadas x e y
+		display.drawString("Hello World", 2, 4);
+		
+		// sair do programa após pressionar qualquer botão
+		buttons.waitForAnyPress();
+	}
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+<p align="center">
+    <img src="https://user-images.githubusercontent.com/58178979/147014815-407d55d8-a506-446c-8869-b14aacb06c4b.png">
+</p>
+
+<p align="center">
+    Acesse aqui todos os códigos-fonte do nosso curso! 📝📝
+</p>
+
+# Copie e cole
+Repositório destinado para guardar todos os códigos implementados durante as aulas. Aqui, você pode copiar/colar sem perder a formatação!
